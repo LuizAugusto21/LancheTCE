b31eed25bfa069c8d247677f3ad4e555f511e91d
Luiz Augusto <luis2848@gmail.com>
2024-07-28 10:45:47 -0300

    feat:SearcBar

--- src/app/src/Pages/home/home.component.ts
+++ src/app/src/Pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../Components/header/header.component';
+import { SearchbarComponent } from '../../Components/searchbar/searchbar.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SearchbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
